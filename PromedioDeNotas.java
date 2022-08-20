868837c3060b364355a6739f84036a8e075631c3
mh0316 <85312998+mh0316@users.noreply.github.com>
2022-08-20 19:55:52 -0400

    Update PromedioDeNotas.java

--- PromedioDeNotas.java
+++ PromedioDeNotas.java
@@ -11,7 +11,7 @@ public class PromedioDeNotas {
         double[] notas = new double[3];
 
         for(int i = 0; i < notas.length; i++){
-            System.out.println("Ingrese la nota (tenga en cuenta las ponderaciones):");
+            System.out.println("Ingrese la nota:");
             notas[i] = ingresar();
         }
         return notas;
